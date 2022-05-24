4b35f971b081b0e529b32fa9352463e39e91e6d7
dkkret <dk.kret@gmail.com>
2022-05-24 18:49:36 +0200

    project done

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+let fileUpload = require('express-fileupload')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.use(fileUpload({
+  createParentPath: true
+}));
+
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  const fileName = req.files.upfile.name
+  const fileType = req.files.upfile.mimetype
+  const fileSize = req.files.upfile.size
+
+  res.json({
+    name: fileName,
+    type: fileType,
+    size: fileSize
+  })
+})
 
 
 
